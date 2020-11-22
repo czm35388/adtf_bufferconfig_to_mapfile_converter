377cc732c2ed373e25ff62107d3aa0869470b153
michael-czernicky <michael.czernicky@digitalwerk.net>
2020-11-22 09:57:11 +0100

    * splitted converter and lib correct, its now working :-)

--- source/app/converter_application.cpp
+++ source/app/converter_application.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-#include "parser_lib.h"
+#include <parser_lib.h>
 
 int main()
 {
     std::cout << "Executed 'converter_applicatoin'" << std::endl;
+
+    cParserLib oParserLib;
+    oParserLib.OpenBufferConfigFile();
 
     return 0;
 }
--- source/lib/parser_lib.cpp
+++ source/lib/parser_lib.cpp
@@ -1,11 +1,13 @@
-#include <iostream>
 #include "parser_lib.h"
+
+#include <iostream>
+#include <pugixml.hpp>
 
 void cParserLib::OpenBufferConfigFile()
 {
     std::cout << "Called cParserLib::OpenXMLFile()" << std::endl;
 
     pugi::xml_document doc;
-    pugi::xml_parse_result result = doc.load_file("tree.xml");
-    std::cout << "Load result: " << result.description() << ", mesh name: " << doc.child("mesh").attribute("name").value() << std::endl;
+    pugi::xml_parse_result result = doc.load_file("c:/adtf/projects/adtf_bufferconfig_map_converter/package/bin/tree.xml");
+    std::cout << "Load result: " << result.description() << ", root type: " << doc.child("root").attribute("type").value() << std::endl;
 }
