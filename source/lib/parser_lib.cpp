b1d61a1a5fa3089e66524856d7702b9d17a1960d
czm35388 <michaelczernicky@web.de>
2021-01-04 23:01:35 +0100

    * added methods for creating and saving new mapfile * check in- and output nodes

--- source/lib/parser_lib.cpp
+++ source/lib/parser_lib.cpp
@@ -4,9 +4,11 @@
 
 bool cParserLib::OpenBufferConfigFile(std::string strInput)
 {
-    pugi::xml_document doc;
-    pugi::xml_parse_result result = doc.load_file(strInput.c_str());
+    pugi::xml_document buffer_config_file;
+    pugi::xml_parse_result result = buffer_config_file.load_file(strInput.c_str());
 
+    m_strInputPath = strInput;
+    std::cout << "Input File: " << m_strInputPath.c_str() << std::endl;
 
     if (!result)
     {
@@ -17,7 +19,7 @@ bool cParserLib::OpenBufferConfigFile(std::string strInput)
         return false;
     }
 
-    pugi::xml_node globals = doc.child("settings");
+    pugi::xml_node globals = buffer_config_file.child("settings");
 
     HandleInputNode(globals.children("input"));
     HandleOutputNode(globals.children("output"));
@@ -34,12 +36,51 @@ bool cParserLib::OpenBufferConfigFile(std::string strInput)
     return true;
 }
 
+void cParserLib::CreateMapFile(std::shared_ptr<pugi::xml_document> pMapfile)
+{
+    pugi::xml_node declaration_node = pMapfile->append_child(pugi::node_declaration);
+
+    declaration_node.append_attribute("version") = "1.0";
+    declaration_node.append_attribute("encoding")   = "ISO-8859-1";
+    declaration_node.append_attribute("standalone") = "no";
+
+    pugi::xml_node mapping_node = pMapfile->append_child("mapping");
+    mapping_node.append_attribute("codec") = "XCP";
+    mapping_node.append_attribute("version") = "2.0";
+}
+
+void cParserLib::SaveMapFile(std::shared_ptr<pugi::xml_document> pMapfile, bool bInputMapfile)
+{
+    std::string strMapfilePath = m_strInputPath;
+    strMapfilePath.resize(strMapfilePath.length() - 4);
+
+    if(bInputMapfile)
+    {
+        strMapfilePath.append("_input.map");
+    }
+    else
+    {
+        strMapfilePath.append("_output.map");
+    }
+
+    pMapfile->save_file(strMapfilePath.c_str(), PUGIXML_TEXT("  "));
+}
+
+
 // Handle XML node methods
 void cParserLib::HandleInputNode(pugi::xml_object_range<pugi::xml_named_node_iterator> input_nodes)
 {
-    for (const auto input_node : input_nodes)
+    if(!m_pInputMapfile->empty())
     {
+        CreateMapFile(m_pInputMapfile);
+    }
+    
+    pugi::xml_node mapping_node = m_pInputMapfile->last_child();
+
+    for (const auto input_node : input_nodes)
+    {   
         m_ui8InputCount +=1;
+        pugi::xml_node map_inputs_node = mapping_node.append_child("inputs");
         
         for (const auto input_attr: input_node.attributes())
         {
@@ -48,10 +89,12 @@ void cParserLib::HandleInputNode(pugi::xml_object_range<pugi::xml_named_node_ite
 
         std::cout << std::endl;
 
-        HandleBufferNode(input_node.children("buffer"));
+        HandleBufferNode(input_node.children("buffer"), map_inputs_node);
 
         std::cout << std::endl;
     }
+
+    SaveMapFile(m_pInputMapfile, true);
 }
 
 void cParserLib::HandleOutputNode(pugi::xml_object_range<pugi::xml_named_node_iterator> output_nodes)
@@ -59,6 +102,8 @@ void cParserLib::HandleOutputNode(pugi::xml_object_range<pugi::xml_named_node_it
     for (const auto output_node : output_nodes)
     {
         m_ui8OutputCount +=1;
+        pugi::xml_node map_outputs_node = m_pOutputMapfile->append_child("outputs");
+
         
         for (const auto output_attr: output_node.attributes())
         {
@@ -66,25 +111,36 @@ void cParserLib::HandleOutputNode(pugi::xml_object_range<pugi::xml_named_node_it
         }
 
         std::cout << std::endl;
+        HandleBufferNode(output_node.children("buffer"), map_outputs_node);
 
-        HandleBufferNode(output_node.children("buffer"));
-
-        std::cout << std::endl;
     }
+
+    SaveMapFile(m_pOutputMapfile, false);
+
 }
 
-void cParserLib::HandleBufferNode(pugi::xml_object_range<pugi::xml_named_node_iterator> buffer_nodes)
+void cParserLib::HandleBufferNode(pugi::xml_object_range<pugi::xml_named_node_iterator> buffer_nodes, pugi::xml_node map_dir_node)
 {
     for (const auto buffer_node : buffer_nodes)
     {
         m_ui8BufferCount += 1;
 
-        for (const auto buffer_attr : buffer_node.attributes())
+        if(std::string("inputs").compare(map_dir_node.value()))
         {
-            std::cout << " " << buffer_attr.name() << "=" << buffer_attr.value();
+            pugi::xml_node map_input_node = map_dir_node.append_child("input");
+            for (const auto buffer_attr : buffer_node.attributes())
+            {
+                map_input_node.append_attribute(buffer_attr.name()) = buffer_attr.value();
+            }
         }
-
-        std::cout << std::endl;
+        else
+        {
+            pugi::xml_node map_output_node = map_dir_node.append_child("output");
+            for (const auto buffer_attr : buffer_node.attributes())
+            {
+                map_output_node.append_attribute(buffer_attr.name()) = buffer_attr.value();
+            }
+        }
 
         HandleStructNode(buffer_node.children("struct"));
 
--- source/lib/parser_lib.h
+++ source/lib/parser_lib.h
@@ -8,11 +8,13 @@ class cParserLib
 {
     public:
         bool OpenBufferConfigFile(std::string strInput);
+        void CreateMapFile(std::shared_ptr<pugi::xml_document> pMapfile);
+        void SaveMapFile(std::shared_ptr<pugi::xml_document> pMapfile, bool bInputMapfile);
 
         // Handle XML node methods
         void HandleInputNode(pugi::xml_object_range<pugi::xml_named_node_iterator> input_nodes);
         void HandleOutputNode(pugi::xml_object_range<pugi::xml_named_node_iterator> output_nodes);
-        void HandleBufferNode(pugi::xml_object_range<pugi::xml_named_node_iterator> buffer_nodes);
+        void HandleBufferNode(pugi::xml_object_range<pugi::xml_named_node_iterator> buffer_nodes, pugi::xml_node map_dir_node);
         void HandleStructNode(pugi::xml_object_range<pugi::xml_named_node_iterator> struct_nodes);
         void HandleElementNode(pugi::xml_object_range<pugi::xml_named_node_iterator> element_nodes);
 
@@ -29,4 +31,9 @@ class cParserLib
         uint8_t m_ui8BufferCount = 0;
         uint8_t m_ui8StructCount = 0;
         uint8_t m_ui8ElementCount = 0;
+
+        std::string m_strInputPath = "";
+
+        std::shared_ptr<pugi::xml_document> m_pInputMapfile = std::make_shared<pugi::xml_document>();
+        std::shared_ptr<pugi::xml_document> m_pOutputMapfile = std::make_shared<pugi::xml_document>();
 };
